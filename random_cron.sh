ff31213010a3e18b243ad417c056f26afd9f64d0
zhoujia123 <452272683@qq.com>
2021-12-03 22:54:59 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -17,8 +17,8 @@
 # else
 #     time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
 # fi
-if [[ ( $(date '+%H') == 00 ) || ( $(date '+%H') == 03 )  || ( $(date '+%H') == 05 )  || ( $(date '+%H') == 07 )  || ( $(date '+%H') == 09 )  || ( $(date '+%H') == 11 )  || ( $(date '+%H') == 13 ) ]] ;then
-    time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
+if [[ ( $(date '+%H') == 17 ) || ( $(date '+%H') == 19 )  || ( $(date '+%H') == 21 )  || ( $(date '+%H') == 23 )  || ( $(date '+%H') == 01 )  || ( $(date '+%H') == 03 )  || ( $(date '+%H') == 06 ) ]] ;then
+    time=$(($RANDOM%10))" 17,19,21,23,1,3,6 * * *"
     echo 当前时间为$(date '+%H')时，计划运行时间$time
     sed -i '/cron:/d' .github/workflows/run.yml
     sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
